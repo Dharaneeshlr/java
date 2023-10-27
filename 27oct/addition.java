09afd2ecf8cd0954dd28041a9816733486b902d8
Dharaneeshlr <149167082+Dharaneeshlr@users.noreply.github.com>
2023-10-27 22:29:58 +0530

    Create addition.java

---
+++ 27oct/addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class addition {
+	public static void main(String[] args){
+		java.util.Scanner sc = new Scanner(System.in);
+				int a =sc.nextInt();
+				int b= sc.nextInt();
+	int c=a+b;
+	  System.out.println(c);
+	}
+
+}
